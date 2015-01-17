df577df4fd6627cada6d7c7b51946dbeb51a1a1e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-17 11:52:43 -0500

    Add base Piece class
    Initialize with algebraic notation position (i.e. "a3") and set color ("white" or "black"). On initialization converts algebraic notation into number coordinates.

---
+++ lib/piece.rb
@@ -0,0 +1,22 @@
+class Piece
+    def initialize(algebraic, set)
+        # TODO: improve algebraic setter interface
+        self.algebraic(algebraic)
+        @set = set.to_sym
+    end
+
+    def algebraic(algebraic)
+        @algebraic = algebraic.to_sym
+        @x, @y = self.decode_algebraic(@algebraic)
+    end
+
+    def decode_algebraic(position)
+        x = self.decode_letter(position[0])
+        y = position[1].to_i
+        return x, y
+    end
+
+    def decode_letter(letter)
+        return letter.bytes[0] - 96
+    end
+end

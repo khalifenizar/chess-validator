399cd22a1f92818eddac6e800966f95fa0a6a6ae
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-17 18:25:13 -0500

    Only allow Pawn to move 2 spaces on first move

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -4,22 +4,25 @@ class Pawn < Piece
     def initialize(algebraic, set)
         super(algebraic, set)
 
-        @direction =
-            case set
-            when :white then 1
-            when :black then -1
-            end
+        case set
+        when :white
+            @direction = 1
+            @starting_y = 2
+        when :black
+            @direction = -1
+            @starting_y = 7
+        end
 
-        @has_moved = false
+        @is_first = @y == @starting_y
     end
 
     def move_to(destination)
         distance_x, distance_y = self.distance(destination)
         direction = distance_y <=> 0
 
-        return distance_x == 0 &&    # needs to not move to the side
-            distance_y.abs <= 2 &&   # needs to not move more than 2 spaces
-            distance_y.abs > 0 &&    # needs to actually move
-            direction == @direction  # needs to move in the correct direction
+        return direction == @direction &&        # needs to move in the correct direction
+            distance_x == 0 &&                   # needs to not move to the side
+            (distance_y.abs == 1 ||              # needs to move one space unless...
+            (distance_y.abs == 2 && @is_first))  # it's the first move and it can move two
     end
 end

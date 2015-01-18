7615b3c2f2c09c7d31f3aafc4918fd9562099c60
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-18 10:30:15 -0500

    Refactor position methods into ChessPosition

---
+++ lib/chess_position.rb
@@ -0,0 +1,23 @@
+class ChessPosition
+    def initialize(algebraic)
+        @algebraic = algebraic.to_sym
+        @x, @y = self.decode_algebraic(@algebraic)
+    end
+
+    def decode_algebraic(position)
+        x = self.decode_letter(position[0])
+        y = position[1].to_i
+        return x, y
+    end
+
+    def decode_letter(letter)
+        return letter.bytes[0] - 96
+    end
+
+    def distance(target)
+        target_x, target_y = self.decode_algebraic(target)
+        x = target_x - @x
+        y = target_y - @y
+        return x, y
+    end
+end
--- lib/piece.rb
+++ lib/piece.rb
@@ -1,28 +1,12 @@
+require_relative "chess_position"
+
 class Piece
     def initialize(algebraic, set)
-        self.algebraic = algebraic
+        @position = ChessPosition.new(algebraic)
         @set = set.to_sym
     end
 
-    def algebraic=(algebraic)
-        @algebraic = algebraic.to_sym
-        @x, @y = self.decode_algebraic(@algebraic)
-    end
-
-    def decode_algebraic(position)
-        x = self.decode_letter(position[0])
-        y = position[1].to_i
-        return x, y
-    end
-
-    def decode_letter(letter)
-        return letter.bytes[0] - 96
-    end
-
     def distance(target)
-        target_x, target_y = self.decode_algebraic(target)
-        x = target_x - @x
-        y = target_y - @y
-        return x, y
+        return @position.distance(target)
     end
 end

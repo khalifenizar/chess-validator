bc6404d3000c7493063fb9cb5d9e640db421038a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-18 11:04:07 -0500

    Add Rook class with #move_to method
    Include the StraightMove module for move calculation.

---
+++ lib/rook.rb
@@ -0,0 +1,10 @@
+require_relative "move/straight_move"
+require_relative "piece"
+
+class Rook < Piece
+    include StraightMove
+
+    def move_to(destination)
+        return self.straight_move?(@position.algebraic, destination)
+    end
+end

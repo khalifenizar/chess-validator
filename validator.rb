dacb0cbe46b5a4947fa74bec9e44f866811f10ca
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-21 00:03:49 -0500

    Read and validate list of moves from a file

--- validator.rb
+++ validator.rb
@@ -1,10 +1,10 @@
-require_relative "lib/chess_board"
+require_relative 'lib/moves/file_move_list'
+require_relative 'lib/chess_board'
 
+moves = FileMoveList.new('moves.txt')
 board = ChessBoard.new
-puts "a2 a3 = #{board.move?(:a2, :a3)}"
-puts "a2 a4 = #{board.move?(:a2, :a4)}"
-puts "a2 a5 = #{board.move?(:a2, :a5)}"
-puts "a7 a6 = #{board.move?(:a7, :a6)}"
-puts "a7 a5 = #{board.move?(:a7, :a5)}"
-puts "a7 a4 = #{board.move?(:a7, :a4)}"
-puts "f5 f7 = #{board.move?(:f5, :f7)}"
+
+moves.each do |start, target|
+    result = board.move?(start, target)
+    puts "#{start} #{target} = #{result}"
+end
